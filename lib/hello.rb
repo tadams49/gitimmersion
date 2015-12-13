c0fe806ec3d51d40833beaf19393474911daeb61
tadams49 <tadams49@uncc.edu>
2015-12-13 21:02:54 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,15 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Thomas Adams (tadams49@uncc.edu)
 name = ARGV.first || "World"
 
+<<<<<<< HEAD
 puts "What's your name"
 my_name = gets.strip
 
 puts "Hello, #{my_name}!"
+=======
+greeter = Greeter.new(name)
+puts greeter.greet
+>>>>>>> Hello uses Greeter
